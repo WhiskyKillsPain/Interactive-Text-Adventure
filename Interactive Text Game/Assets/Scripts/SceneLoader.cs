42d3f9183b51503477d5f92d4f2149a67dfd5786
WhiskyKillsPain <111146078+WhiskyKillsPain@users.noreply.github.com>
2022-09-16 12:08:30 -0700

    commit 7

--- Interactive Text Game/Assets/Scripts/SceneLoader.cs
+++ Interactive Text Game/Assets/Scripts/SceneLoader.cs
@@ -9,6 +9,7 @@ public class SceneLoader : MonoBehaviour
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
     // Update is called once per frame
